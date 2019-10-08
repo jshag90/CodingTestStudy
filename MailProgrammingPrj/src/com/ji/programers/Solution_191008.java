bfa7b31689905a523ccb9f31eb79e089b6fe52a9
jiseonhak <jshag90@naver.com>
2019-10-08 20:43:45 +0900

    프로그래머스 알고리즘 문제해결 자릿수 더하기 정답 및 풀이

--- MailProgrammingPrj/src/com/ji/programers/Solution_191008.java
+++ MailProgrammingPrj/src/com/ji/programers/Solution_191008.java
@@ -2,36 +2,25 @@ package com.ji.programers;
 
 public class Solution_191008 {
 	public static void main(String[] args) {
-//		System.out.println();
-//		System.out.println(solution(421));
-		solution(1421);
-		System.out.println(solution(1421));
+		System.out.println(solution(42119));
 	}
-
-	static int seperateNumSum = 0;
 
 	public static int solution(int n) {
 		int answer = 0;
+		
 		int seperateNum = 0;
 		int numPosition = (int) (Math.log10(n) + 1);
 
-		// System.out.println(n/(10*numPosition));
-		int powValue = (int) Math.pow(10, numPosition - 1);
-		seperateNum = n / powValue;
-		// System.out.println(seperateNum);
-		seperateNumSum += seperateNum;
-		int nextNum = n - (seperateNum * powValue);
-		// System.out.println(nextNum);
-		// System.out.println(seperateNumSum);
-		if (numPosition > 1) {
-			solution(nextNum);
-			return answer;
-		}else{
-//			System.out.println(seperateNumSum);
-			answer = seperateNumSum;
-			return seperateNumSum;
+		for (int index = numPosition; index> 0; index--) {
+			int powValue = (int) Math.pow(10, index - 1);
+			
+			seperateNum = n / powValue;
+			n -= seperateNum * powValue;
+			
+			answer += seperateNum;
 		}
 
-//		return answer;
+		return answer;
 	}
+
 }
