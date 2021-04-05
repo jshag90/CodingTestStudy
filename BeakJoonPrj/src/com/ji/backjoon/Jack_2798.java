6b4876d2497d305616e740c0462b092f479d15be
jshag90 <jshag90@naver.com>
2021-04-05 23:42:57 +0900

    Add Jac problem class

---
+++ BeakJoonPrj/src/com/ji/backjoon/Jack_2798.java
@@ -0,0 +1,36 @@
+package com.ji.backjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Jack_2798 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String cardCounts = br.readLine();
+		String[] inputCountSplite = cardCounts.split(" ");
+		Integer cardCount = Integer.parseInt(inputCountSplite[0]);
+		Integer cardMaxValue = Integer.parseInt(inputCountSplite[1]);
+		
+		System.out.println(cardCount);
+		System.out.println(cardMaxValue);
+		
+		String cardNumbers = br.readLine();
+		String[] inputNumbersSplite = cardCounts.split(" ");
+		
+		for(String number : inputNumbersSplite) {
+		System.out.println(number);
+		}
+
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
