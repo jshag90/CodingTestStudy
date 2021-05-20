5c7b9a3c6059799f028ac65403b6bd3c03425cdf
SeonHakJi <jshag90@naver.com>
2021-05-20 22:22:47 +0900

    [Modify] RecommNewId Class

--- Programmers/com/ji/study/RecommNewId.java
+++ Programmers/com/ji/study/RecommNewId.java
@@ -10,21 +10,14 @@ public class RecommNewId {
 		String answer = "";
 
 		new_id = parseToLowerCase(new_id);
-		System.out.println(new_id);
 		new_id = removeSpecialChar(new_id);
-		System.out.println(new_id);
 		new_id = replaceComma(new_id);
-		System.out.println(new_id);
 		new_id = removeFirstComma(new_id);
-		System.out.println(new_id);
 		new_id = removeBlank(new_id);
-		System.out.println(new_id);
 		new_id = removeLengthOver(new_id);
-		System.out.println(new_id);
 		new_id = removeLengthLow(new_id);
-		System.out.println(new_id);
-
 		answer = new_id;
+		
 		return answer;
 	}
 
@@ -33,11 +26,11 @@ public class RecommNewId {
 	}
 
 	public static String removeSpecialChar(String newId) {
-		String regex = "[0-9]*$|[a-z]*$";
+		String regex = "[0-9]*$|[a-z]*$|[-]|[_]|[.]";
 		String filterStr = "";
 		for (int i = 0; i < newId.length(); i++) {
 			String charStr = newId.substring(i, i + 1);
-			if (charStr.matches(regex) || charStr.equals("-") || charStr.equals("_") || charStr.equals(".")) {
+			if (charStr.matches(regex)) {
 				filterStr += charStr;
 			}
 		}
@@ -45,37 +38,43 @@ public class RecommNewId {
 	}
 
 	public static String replaceComma(String newId) {
-		String parseNewId = newId.replace("...", ".");
-		return parseNewId.replace("..", ".");
+		return newId.replace("...", ".").replace("..", ".");
 	}
 
 	public static String removeFirstComma(String newId) {
 		if (newId.substring(0, 1).equals("."))
 			newId = newId.substring(1, newId.length());
-		if (newId.substring(newId.length() - 1, newId.length()).equals("."))
+		
+		if (!newId.equals("") &&newId.substring(newId.length()-1).equals("."))
 			newId = newId.substring(0, newId.length() - 1);
+			
 		return newId;
 	}
 
 	public static String removeBlank(String newId) {
-		return newId.replace(" ", "");
+		newId = newId.replace(" ", "");
+		if(newId.equals(""))
+			newId+="a";
+		return newId;
 	}
 
 	public static String removeLengthOver(String newId) {
-		if (newId.length() > 15)
-			return newId.substring(0, 15);
-		else
+		
+		if (newId.length() > 15) {
+			String removeNewId = newId.substring(0, 15);
+			return removeFirstComma(removeNewId);
+		}else {
 			return newId;
+		}
+		
 	}
 
 	public static String removeLengthLow(String newId) {
 
 		if (newId.length() <= 2) {
-
 			String lastChar = newId.substring(newId.length() - 1, newId.length());
-			while (newId.length() < 3) {
+			while (newId.length() < 3) 
 				newId += lastChar;
-			}
 		}
 
 		return newId;
