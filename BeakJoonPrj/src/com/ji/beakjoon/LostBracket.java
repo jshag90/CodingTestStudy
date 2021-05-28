2f3dc5d6908b2e6f2e4eee222bba77c24155f3f5
SeonHakJi <jshag90@naver.com>
2021-05-28 23:38:34 +0900

    [Add] LostBracket class, MakeNumberOne

--- BeakJoonPrj/src/com/ji/beakjoon/LostBracket.java
+++ BeakJoonPrj/src/com/ji/beakjoon/LostBracket.java
@@ -5,101 +5,40 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
-
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 
 public class LostBracket {
 
-	static List<Integer> sumNumArr = new ArrayList<Integer>();
-	static Integer minVal = 0;
-	public static void main(String[] args) throws IOException, ScriptException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		ScriptEngineManager manager = new ScriptEngineManager();
-		ScriptEngine engine = manager.getEngineByName("JavaScript");
 
 		
 		String calcuFormula = String.valueOf(br.readLine());
-		char[] calcu = calcuFormula.toCharArray();
+		int result = Integer.MAX_VALUE;
 		
-		List<String> formulaList = new ArrayList<>();
-		for(int i=0; i<calcu.length; i++) {
-			String calcuStr = String.valueOf(calcu[i]);
-			boolean regex = Pattern.matches("[+]|[-]", calcuStr);
-			if(regex) 
-				formulaList.add(calcuStr);
+		// 마이너스 기준으로 순자 분리
+		String[] plusNumArr = calcuFormula.split("-");
+
+		for (String numbersStr : plusNumArr) {
+			int temp = 0;
+
+			// 플러스가 포함된 수식의 경우 계산하여 temp에 저장
+			String[] formula = numbersStr.split("\\+");
+			for (String formulas : formula)
+				temp += Integer.parseInt(formulas);
+
+			// 첫번째 숫자로 초기화
+			// Q)  Integer.MAX_VALUE 로 초기 값을 해야하는 이유???? 
+			result = (result == Integer.MAX_VALUE) ? temp: result-temp;
+
 		}
 
-		String[] numbers = calcuFormula.split("[+]|[-]");
-		
-		
-		int inputLen = numbers.length;
-		
-		int[] inputNumbers = new int[inputLen];
-		for (int i = 0; i < inputLen; i++) {
-			inputNumbers[i] = Integer.valueOf(numbers[i]);
-		}
-
-		int[] output = new int[inputLen];
-		boolean[] visited = new boolean[inputLen];
-		List<int[]> makeNumber = new ArrayList<int[]>();
-		dfs(0, initNodeValue(inputNumbers, inputLen), new int[inputLen], new boolean[inputLen], formulaList, engine);
-
-//		for(int[] arr : makeNumber) {
-//			for (int i = 0; i < arr.length; i++) {
-//				System.out.print(arr[i] + " ");
-//			}
-//			System.out.println();
-//		}
-
-//		bw.write("result : " + engine.eval(calcuFormula));
-		bw.write("result : " + minVal);
+	
+		bw.write(String.valueOf(result));
 
 		br.close();
 		bw.flush();
 		bw.close();
-	}
-
-	public static int[] initNodeValue(int[] numToArr, Integer numLength) {
-		int[] result = new int[numLength];
-		for (int i = 0; i < numLength; i++) {
-			String numStr = String.valueOf(numToArr[i]);
-			result[i] = Integer.valueOf(numStr);
-		}
-		return result;
-	}
-
-	public static void dfs(int n, int[] arr, int[] result, boolean[] visit, List<String> formulaList , ScriptEngine engine) throws ScriptException {
-		if (n == arr.length) {// 입력된 숫자 길이 만큼의 경우의 숫자들
-			
-			String formulaStr = "";
-			for(int i = 0; i< result.length-1 ;i++) {
-				formulaStr += result[i]+formulaList.get(i);
-			}
-			formulaStr+=result[result.length-1];
-			System.out.println(formulaStr);
-			int calcu = (int)engine.eval(formulaStr);
-			System.out.println(calcu);
-			if( minVal >= (int)engine.eval(formulaStr))
-				minVal = (int)engine.eval(formulaStr);
-
-		} else {
-
-			for (int i = 0; i < arr.length; i++) {
-				if (!visit[i]) {
-					visit[i] = true;
-					result[n] = arr[i];
-					dfs(n + 1, arr, result, visit, formulaList, engine);
-					visit[i] = false;
-				}
-			}
-
-		}
 	}
 
 }
--- BeakJoonPrj/src/com/ji/beakjoon/MakeNumberOne.java
+++ BeakJoonPrj/src/com/ji/beakjoon/MakeNumberOne.java
@@ -5,28 +5,23 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class MakeNumberOne {
 
-	public static int calcuCount = 0;
-
+	static Integer[] dp;
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int inputNumber = Integer.valueOf(br.readLine());
-		List<Integer> resultList = new ArrayList<Integer>();
-		makeNumberOne(inputNumber);
-		resultList.add(calcuCount);
-		calcuCount = 0;
-		makeNumberOne(inputNumber-1);
-		resultList.add(calcuCount);
 		
-		bw.write("result : " + Collections.min(resultList));
+		dp = new Integer[inputNumber + 1];
+		dp[0] = dp[1] = 0;
+		
+		int result = recur(inputNumber);
+		
+		bw.write(String.valueOf(result));
 
 		br.close();
 		bw.flush();
@@ -34,21 +29,38 @@ public class MakeNumberOne {
 
 	}
 
-	public static void makeNumberOne(int inputNumber) {
-		System.out.println(inputNumber);
-
-		if (inputNumber % 3 == 0) {
-			inputNumber = inputNumber / 3;
-			calcuCount++;
-			if (inputNumber != 1)
-				makeNumberOne(inputNumber);
-		} else if (inputNumber % 2 == 0) {
-			inputNumber = inputNumber / 2;
-			calcuCount++;
-			if (inputNumber != 1)
-				makeNumberOne(inputNumber);
+	public static int recur(int N) {
+		 
+		if (dp[N] == null) {
+			// 6으로 나눠지는 경우 
+			if (N % 6 == 0) {
+				dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
+			}
+			// 3으로만 나눠지는 경우 
+			else if (N % 3 == 0) {
+				dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
+			}
+			// 2로만 나눠지는 경우 
+			else if (N % 2 == 0) {
+				dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
+			}
+			// 2와 3으로 나누어지지 않는 경우
+			else {
+				dp[N] = recur(N - 1) + 1;
+			}
 		}
-
+		
+		return dp[N];
 	}
+	
+
+//	static int recur(int N, int count) {
+//		// N이 2 미만인 경우 누적된 count값을 반환
+//		if (N < 2) {
+//			return count;
+//		}
+//		return Math.min(recur(N / 2, count + 1 + (N % 2)), recur(N / 3, count + 1 + (N % 3)));
+// 
+//	}
 
 }
