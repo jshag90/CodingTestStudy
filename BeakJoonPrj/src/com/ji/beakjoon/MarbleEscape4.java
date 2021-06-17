da3d74a21858b949ea9df00c72bd211ce075910e
SeonHakJi <jshag90@naver.com>
2021-06-17 23:27:30 +0900

    [Add] BeakJoon MarbleEscape4 Try Solve

---
+++ BeakJoonPrj/src/com/ji/beakjoon/MarbleEscape4.java
@@ -0,0 +1,91 @@
+package com.ji.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class MarbleEscape4 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int[] dy = { -1, 1, 0, 0 };//상하좌우위아래
+        int[] dx = { 0, 0, -1, 1 };
+        int M = sc.nextInt(), N = sc.nextInt();
+
+        char[][] board = new char[N][M];
+        int cnt = 0, days = 0;
+        Queue<int[]> redQue = new LinkedList<>();
+        Queue<int[]> blueQue = new LinkedList<>();
+        int[] holePosition = new int[1];
+
+        //행렬값 초기화
+        for (int n = 0; n < N; n++) {
+        	String line = br.readLine();
+        	char[] lineArr = line.toCharArray();
+            for (int m = 0; m < M; m++) {
+            	System.out.print(lineArr[m]+" ");
+            	board[n][m] = lineArr[m];
+            	
+            	if(board[n][m] == 'R') {
+            		redQue.add(new int[] {n, m});
+            	}
+            	if(board[n][m] == 'B') {
+            		blueQue.add(new int[] {n, m});
+            	}
+            	
+            	if(board[n][m] == 'O') {
+            		holePosition[0] = n;
+            		holePosition[1] = m;
+            	}
+            	
+            }
+            
+        }
+        
+//        for (int n = 0; n < N; n++) {
+//            for (int m = 0; m < M; m++) {
+//            	System.out.print(board[n][m] + "");
+//            }
+//            System.out.println();
+//        }
+        
+//        //익지 않은 토마토 가 있고, 큐에 노드가 있으면
+//        while (cnt > 0 && !que.isEmpty()) {
+//            for (int s = que.size(); s > 0; s--) {
+//                int[] cur = que.poll();//익은 토마토 위치
+//
+//                //상하좌우아래 검사
+//                for (int k = 0; k < 4; k++) {
+//                    int ny = cur[0] + dy[k]; //가로
+//                    int nx = cur[1] + dx[k]; //세로
+//
+//                    //익은 토마토만 통과
+//                    if (ny < 0 || nx < 0 || ny >= N || nx >= M || tomato[ny][nx] != 0)
+//                        continue;
+//                               
+//                    cnt--;
+//                    tomato[ny][nx] = 1;
+//                    //익은 토마토 큐에 저장
+//                    que.add(new int[] { ny, nx });
+//                }
+//            }
+//            //날짜 증가
+//            days++;
+//        }
+        
+        String result = String.valueOf(cnt == 0 ? days : -1);
+        bw.write(String.valueOf(result) + "\n");
+        bw.flush();
+		bw.close();
+		
+	}
+
+}
--- BeakJoonPrj/src/com/ji/beakjoon/TreeTraversal.java
+++ BeakJoonPrj/src/com/ji/beakjoon/TreeTraversal.java
@@ -11,79 +11,90 @@ public class TreeTraversal {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int nodeCount = Integer.valueOf(br.readLine());
+//		int nodeCount = Integer.valueOf(br.readLine());
+//
+//		List<BinaryTreeNode> bTreeNodeList = new ArrayList<BinaryTreeNode>();
+//		for (int i = 0; i < nodeCount ; i++) {
+//			String[] nodeInfo = br.readLine().split(" ");
+//			BinaryTreeNode binaryTreeNode = new BinaryTreeNode();
+//			binaryTreeNode.setData(nodeInfo[0]);
+//
+//			BinaryTreeNode leftTreeNode = new BinaryTreeNode();
+//			BinaryTreeNode rightTreeNode = new BinaryTreeNode();
+//			leftTreeNode.setData(nodeInfo[1].equals(".") ? null : nodeInfo[1]);
+//			rightTreeNode.setData(nodeInfo[2].equals(".") ? null : nodeInfo[2]);
+//
+//			binaryTreeNode.setLeftNode(leftTreeNode);
+//			binaryTreeNode.setRightNode(rightTreeNode);
+//
+//			bTreeNodeList.add(binaryTreeNode);
+//		}
+//		
+//		for(BinaryTreeNode node : bTreeNodeList) {
+//			System.out.println(node.getData());
+//		}
+//
+//	}
 
-		List<BinaryTreeNode> bTreeNodeList = new ArrayList<BinaryTreeNode>();
-		for (int i = 0; i < nodeCount ; i++) {
-			String[] nodeInfo = br.readLine().split(" ");
-			BinaryTreeNode binaryTreeNode = new BinaryTreeNode();
-			binaryTreeNode.setData(nodeInfo[0]);
+//	static void PreOrder(List<BinaryTreeNode> bTreeNodeList, BinaryTreeNode root) {
+//		if (root != null) {
+//			System.out.println(root.getData() + " ");
+//			PreOrder(bTreeNodeList, root.getLeftNode());
+//			PreOrder(bTreeNodeList, root.getRightNode());
+//		}
+//	}
 
-			BinaryTreeNode leftTreeNode = new BinaryTreeNode();
-			BinaryTreeNode rightTreeNode = new BinaryTreeNode();
-			leftTreeNode.setData(nodeInfo[1].equals(".") ? null : nodeInfo[1]);
-			rightTreeNode.setData(nodeInfo[2].equals(".") ? null : nodeInfo[2]);
-
-			binaryTreeNode.setLeftNode(leftTreeNode);
-			binaryTreeNode.setRightNode(rightTreeNode);
-
-			bTreeNodeList.add(binaryTreeNode);
-		}
-		
-		for(BinaryTreeNode node : bTreeNodeList) {
-			System.out.println(node.getData());
-		}
-
-	}
-
-	static void PreOrder(List<BinaryTreeNode> bTreeNodeList, BinaryTreeNode root) {
-		if (root != null) {
-			System.out.println(root.getData() + " ");
-			PreOrder(bTreeNodeList, root.getLeftNode());
-			PreOrder(bTreeNodeList, root.getRightNode());
-		}
-	}
-
-	public BinaryTreeNode getNodeByData(List<BinaryTreeNode> bTreeNodeList, String data) {
-		BinaryTreeNode result = new BinaryTreeNode();
-		for (BinaryTreeNode treeNode : bTreeNodeList) {
-			if (treeNode.getData().equals(data)) {
-				result = treeNode;
-				break;
-			}
-		}
-		return result;
-	}
+//	public BinaryTreeNode getNodeByData(List<BinaryTreeNode> bTreeNodeList, String data) {
+//		BinaryTreeNode result = new BinaryTreeNode();
+//		for (BinaryTreeNode treeNode : bTreeNodeList) {
+//			if (treeNode.getData().equals(data)) {
+//				result = treeNode;
+//				break;
+//			}
+//		}
+//		return result;
+//	}
 
 }
 
-class BinaryTreeNode {
-	public String data;
-	public BinaryTreeNode leftNode;
-	public BinaryTreeNode rightNode;
-
+class Node{
+	private String data;
+	private Node nextNode;
+	
+	public Node() {
+		this.data = null;
+		this.nextNode = null;
+	}
+	
+	public Node(String data) {
+		this.data = data;
+		this.nextNode = null;
+	}
+	
+	public Node(String data, Node nextNode) {
+		this.data = data; 
+		this.nextNode = nextNode;
+	}
+	
 	public String getData() {
-		return data;
+		return this.data;
+	}
+
+	public Node getNextNode() {
+		return nextNode;
+	}
+
+	public void setNextNode(Node nextNode) {
+		this.nextNode = nextNode;
 	}
 
 	public void setData(String data) {
 		this.data = data;
 	}
+	
+}
 
-	public BinaryTreeNode getLeftNode() {
-		return leftNode;
-	}
 
-	public void setLeftNode(BinaryTreeNode leftNode) {
-		this.leftNode = leftNode;
-	}
-
-	public BinaryTreeNode getRightNode() {
-		return rightNode;
-	}
-
-	public void setRightNode(BinaryTreeNode rightNode) {
-		this.rightNode = rightNode;
-	}
+	
 
 }
