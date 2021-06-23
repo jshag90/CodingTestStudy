5b2d4237bd0141418da3b706809c9e029c7b5af9
SeonHakJi <jshag90@naver.com>
2021-06-23 23:17:41 +0900

    [Fix] BeakJoon AssingClassroom Solve

--- BeakJoonPrj/src/com/ji/beakjoon/AssignClassroom.java
+++ BeakJoonPrj/src/com/ji/beakjoon/AssignClassroom.java
@@ -6,66 +6,68 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
-/**
- * 
- * 1. Input Data를 2차원 배열로 받는다. (new int[n][2])
-	2. 입력 데이터를 오름차순으로 정렬해준다. (시작 시간이 같다면, 끝나는 시간을 오름차순으로 정렬)
-	3. PriorityQueue(우선순위 큐)를 만들어주고, (정렬된)배열의 첫 번째 end값을 큐에 넣는다.
-	4. 배열을 두 번째 값부터 순회하면서, start가 PriorityQueue의 peek()보다 작거나 같다면, pq에서 하나 뺀다.
-	4-1. 순회하면서, 현재 end값을 새로 pq에 넣어준다.
-	5. pq에 남아있는 데이터의 갯수가 필요한 강의실의 갯수이다.
- *
- */
 public class AssignClassroom {
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		//입력값 초기화
 		Integer N = Integer.valueOf(br.readLine());
 		List<Lecture> lectureList = new ArrayList<>();
-		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-		for(int i=0; i<N; i++) {
+		for (int i = 0; i < N; i++) {
 			String[] lectureInfo = br.readLine().split(" ");
 			lectureList.add(new Lecture(Integer.parseInt(lectureInfo[0]), Integer.parseInt(lectureInfo[1])));
 		}
+
+		//시작 시간을 기준으로 오름차순 정렬
+		Collections.sort(lectureList, new Comparator<Lecture>() {
+			@Override
+			public int compare(Lecture s1, Lecture s2) {
+				if (s1.getStartTime() == s2.getStartTime()) {
+						return s1.getEndTime() - s2.getEndTime();
+				}
+				return s1.getStartTime() - s2.getStartTime();
+			}
+		});
 		
-		for(Lecture lecture : lectureList) {
-			System.out.println(lecture.getStartTime()+" "+lecture.getEndTime());
+		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+		priorityQueue.add(lectureList.get(0).getEndTime());
+
+		for (int i = 1; i < N; i++) {
+			//(즉, Ti ≤ Sj 일 경우 i 수업과 j 수업은 같이 들을 수 있다.)
+			//위 조건에 의해서 큐안에 끝나는 시간 보다 앞에 있는 수업이라면 같이 들을 수 있게 됨
+			if (priorityQueue.peek() <= lectureList.get(i).getStartTime())
+				priorityQueue.poll();//같은 강의실 수업으로 배정
+
+			//강의 끝나는 시간 큐에 저장이 새로운 강의실 배정을 하게 되는 것
+			priorityQueue.add(lectureList.get(i).getEndTime());
 		}
-		
-		priorityQueue.offer(lectureList.get(0).getEndTime());
-		for(int i=1; i< lectureList.size(); i++) {
-			if(priorityQueue.peek() >= lectureList.get(i).getStartTime())
-				priorityQueue.poll();
-			else
-				priorityQueue.offer(lectureList.get(i).getEndTime());
-		}
-		
-		int result = priorityQueue.size();
-		
-		bw.write(String.valueOf(result));
-		br.close();
+
+		//결국 큐의 크기가 
+		bw.write(String.valueOf(priorityQueue.size()) + "\n");
 		bw.flush();
 		bw.close();
+		br.close();
 	}
-	
 
 }
 
-class Lecture implements Comparable<Lecture> {
+class Lecture {
 	Integer startTime;
 	Integer endTime;
-	
+
 	public Lecture(Integer startTime, Integer endTime) {
 		super();
 		this.startTime = startTime;
 		this.endTime = endTime;
 	}
-	
+
 	public Integer getStartTime() {
 		return startTime;
 	}
@@ -82,17 +84,4 @@ class Lecture implements Comparable<Lecture> {
 		this.endTime = endTime;
 	}
 
-	@Override
-	public int compareTo(Lecture target) {
-		if(target.startTime > this.startTime) {
-			return 1;
-		} else if(target.startTime == this.startTime) {
-			if(target.endTime < this.endTime) { 
-				return 1;
-			}
-		}
-		return -1;
-	}
-
 }
-
