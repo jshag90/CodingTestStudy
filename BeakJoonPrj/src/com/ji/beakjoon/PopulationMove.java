78877ae42e45eaf4fce2001b36bebdd4647277d9
SeonHakJi <jshag90@naver.com>
2021-06-25 23:55:07 +0900

    [Add] BeakJoon PopulationMove, Bomberman solve

--- BeakJoonPrj/src/com/ji/beakjoon/PopulationMove.java
+++ BeakJoonPrj/src/com/ji/beakjoon/PopulationMove.java
@@ -1,125 +1,130 @@
 package com.ji.beakjoon;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-
-/**
- * 1. 2차원 배열에 인구 정보 초기화 
- * 2. 인접해 있는 정수들의 차이가 L이상 R이하인지 검사 
- * 3. 1) 통과시 검사해서 조건에 맞으면 큐안에 넣어줌 
- * 		2) 통과 안되면 패스 
- * 4. 1)의 경우 큐안에 있는 인구들의 합/큐 크기 하여 나온 값을 원래 2차원 배열에 저장 위 경우
- * 반복하면 반복할 경우 인구이동 카운트 증가
- * 
- * @author ji
- *
- */
+import java.util.StringTokenizer;
+ 
 public class PopulationMove {
-
-	static int nodeNum, edgeNum, startNode;
-	static int[][] graph;
-	static boolean[] BFSisVisited;
-	static ArrayList<Integer> BFSvisitArr;
-
-	public static void main(String[] args) throws IOException {
-
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String[] initInfo = br.readLine().split(" ");
-		int countryCount = Integer.valueOf(initInfo[0]);
-		int minPop =  Integer.valueOf(initInfo[1]);
-		int maxPop = Integer.valueOf(initInfo[2]);
-		
-		Queue<int[]> que = new LinkedList<>();
-		int[] dy = { -1, 1, 0, 0 };// 상하좌우위아래
-		int[] dx = { 0, 0, -1, 1 };
-
-		graph = new int[countryCount][countryCount];
-
-		for (int i = 0; i < countryCount; i++) {
-			String[] populationInfo = br.readLine().split(" ");
-			for (int j = 0; j < countryCount; j++) {
-				graph[i][j] = Integer.parseInt(populationInfo[j]);
-				
-			}
-		}
-		
-		
-		for (int i = 0; i < countryCount-1; i++) {
-			for (int j = 0; j < countryCount-1; j++) {
-				
-				int currContry = graph[i][j];
-				if(j+1 < countryCount) {
-					int diffH = Math.abs(currContry - graph[i][j+1] );
-					if(diffH >=  minPop && diffH <= maxPop) {
-						que.add(new int[]{i,j});
-						que.add(new int[]{i, j+1});
-					}
-				}
-				
-				if(i+1 < countryCount) {
-					int diffV = Math.abs(currContry - graph[i+1][j] );
-					if(diffV >=  minPop && diffV <= maxPop) {
-						que.add(new int[]{i,j});
-						que.add(new int[]{i+1, j});
-					}
-				}
-			}
-		}
-		
-		//인접 행렬의 차이를 계산
-//	     while (!que.isEmpty()) {
-//	    	 
-//	    	  int[] cur = que.poll();//익은 토마토 위치
-//	    	  
-//	    	  for (int k = 0; k < 4; k++) {
-//                  int ny = cur[0] + dy[k]; //가로
-//                  int nx = cur[1] + dx[k]; //세로
-//                  
-//                  if (ny < 0 || nx < 0 || ny >= countryCount || nx >= countryCount )
-//                      continue;
-//                  
-////                  System.out.println(ny);
-//////                  System.out.println(nx);
-//                  System.out.println(graph[nx][ny]);
-//                  
-//	    	  }
-//	    	 
-//	     }
-
-//		for (int i = 0; i < countryCount; i++) {
-//			for (int j = 0; j < countryCount; j++) {
-//				System.out.print(graph[i][j] + " ");
-//			}
-//			System.out.println();
-//		}
-
-		
-		bw.flush();
-		bw.close();
-	}
-
-//	static void bfs(int node) {
-//		// 방문한 노드 처리
-//		BFSisVisited[node] = true;
-//		// 방문한 노드 번호 리스트 저장
-//		BFSvisitArr.add(node);
-//		for (int i = 1; i <= nodeNum; i++) {
-//			// 간선이면서, 방문한 노드가 아니고, 검색해야할 노드 큐에 저장해준다.
-//			if (graph[node][i] == 1 && BFSisVisited[i] == false && queue.contains(i) == false) {
-//				queue.add(i);
-//			}
-//		}
-//
-//		// 검색 큐가 비어있지 않으면 검색 큐에서 삭제해준 후 제 검색
-//		if (!queue.isEmpty())
-//			bfs(queue.poll());
-//	}
+ 
+    static int N, L, R, map[][];
+    static boolean visited[][];
+    static int[] dr = {-1, 0, 1, 0}, dc = {0, -1, 0, 1};
+    static Queue<Point> q;
+    static List<Point> group; 
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken()); // 땅 크기
+        L = Integer.parseInt(st.nextToken()); // 인구 차이(이상)
+        R = Integer.parseInt(st.nextToken()); // 인구 차이(이하)
+        
+        map = new int[N][N];
+        group = new LinkedList<>();
+        q = new LinkedList<>();
+ 
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        System.out.println(process());
+    }
+ 
+    private static int process() {
+ 
+        int cnt = 0; // 인구 이동 발생 수
+        boolean isMove = false;
+        
+        while(true) {
+ 
+            visited = new boolean[N][N];
+            isMove = false;
+            
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < N; c++) {
+                    // 이미 확인한 나라 pass
+                    if(visited[r][c]) 
+                    	continue;
+                    
+                    // 연합국을 확인해보자.
+                    if(open(r, c)) 
+                    	isMove = true;
+                }
+            }
+            // 연합국이 존재한다면 인구 이동 발생
+            if(isMove) cnt++;
+            else return cnt;
+        }
+    }
+ 
+    private static boolean open(int r, int c) {
+ 
+        q.clear();
+        group.clear();
+        
+        q.add(new Point(r, c));
+        group.add(new Point(r, c));
+        visited[r][c] = true;
+        
+        int sum = map[r][c];
+        // 연합국을 찾으러
+        while(!q.isEmpty()) {
+            
+            Point now = q.poll();
+            
+            for (int d = 0; d < 4; d++) {
+                int rr = now.r + dr[d];
+                int cc = now.c + dc[d];
+                // 범위를 초과하거나 이미 방문한 국가 pass
+                if(rr < 0 || cc < 0 || rr >= N || cc >= N || visited[rr][cc]) continue;
+                
+                // 두 나라의 인구 차이가 L명 이상, R명 이하가 아니라면 pass
+                int diff = Math.abs(map[now.r][now.c] - map[rr][cc]);
+                if(diff < L || diff > R) 
+                	continue;
+                
+                // OPEN !!!!!
+                // 연합의 인구수와 국가 정보 저장
+                sum += map[rr][cc];
+                q.add(new Point(rr, cc));
+                group.add(new Point(rr, cc));
+                visited[rr][cc] = true;
+            }
+            
+        }
+ 
+        // 연합국이 존재하지 않다면
+        if(group.size() == 1) return false;
+        else {
+        	
+            // 연합을 이루고 있는 각 칸의 인구수는 (연합의 인구수) / (연합을 이루고 있는 칸의 개수)
+            int tmp = sum / group.size(); 
+            for (Point p : group) {
+                map[p.r][p.c] = tmp;
+            }
+            
+            return true;
+        }
+    }
+    
+    static class Point {
+        int r, c;
+ 
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+        
+    }
 }
+
+
