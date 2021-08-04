b99596abc0e8b5e743f149d9cc63ff87fad08b8d
SeonHakJi <jshag90@naver.com>
2021-08-04 23:18:41 +0900

    [Add] Programmers GymSuit Class

---
+++ BeakJoonPrj/src/com/ji/beakjoon/greedy/GymSuit.java
@@ -0,0 +1,14 @@
+package com.ji.beakjoon.greedy;
+
+public class GymSuit {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        return answer;
+    }
+
+}
