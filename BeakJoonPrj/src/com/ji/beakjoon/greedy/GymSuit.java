31dcbf862664fbe151bd0c8e1471d96e47e28123
SeonHakJi <jshag90@naver.com>
2021-08-06 22:42:10 +0900

    [Fix] Programmer Network Solve

--- BeakJoonPrj/src/com/ji/beakjoon/greedy/GymSuit.java
+++ BeakJoonPrj/src/com/ji/beakjoon/greedy/GymSuit.java
@@ -8,13 +8,16 @@ public class GymSuit {
 
 	public static void main(String[] args) {
 		System.out.println(solution(5, new int[] { 2, 4 }, new int[] {1, 3, 5}));
-//		System.out.println(solution(5, new int[] { 2, 4 }, new int[] {3}));
-//		System.out.println(solution(3, new int[] { 3 }, new int[] {1}));
+		System.out.println(solution(5, new int[] { 2, 4 }, new int[] {3}));
+		System.out.println(solution(3, new int[] { 3 }, new int[] {1}));
 	}
 
 	public static int solution(int n, int[] lost, int[] reserve) {
 		int answer = 0;
 
+		Arrays.sort(lost);
+		Arrays.sort(reserve);
+		
 		List<PersonVO> sendPossibleList = new ArrayList<PersonVO>();
 		for (int i = 1; i <= n; i++) {
 
@@ -31,7 +34,7 @@ public class GymSuit {
 			sendPossibleList.add(personVO);
 		}
 
-		for (int i = 1; i < sendPossibleList.size()-1; i++) {
+		for (int i = 1; i < sendPossibleList.size(); i++) {
 
 			if (sendPossibleList.get(i).getTotal() > 1 && sendPossibleList.get(i - 1).getTotal() == 0) {
 				sendPossibleList.get(i - 1).setTotal(1);
