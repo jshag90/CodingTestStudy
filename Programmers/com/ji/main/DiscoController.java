8fb7605186c32f1cdc66ad7380f239db79a3adf1
SeonHakJi <jshag90@naver.com>
2021-08-19 23:42:26 +0900

    [Fix] Programmer DiscoController

--- Programmers/com/ji/main/DiscoController.java
+++ Programmers/com/ji/main/DiscoController.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class DiscoController {
 
 	public static void main(String[] args) {
-		solution(new int[][] {{0,3},{1,9},{2,6}});
+		int result = solution(new int[][] { { 0, 3 }, { 1, 9 }, { 2, 6 } });
+		System.out.println(result);
 	}
 
 	/**
@@ -19,38 +20,72 @@ public class DiscoController {
 	 */
 	public static int solution(int[][] jobs) {
 		int answer = 0;
-
+		
+		
 		List<TaskVO> taskList = new ArrayList<>();
+		int beforeTaskEnd = 0;
 		for (int i = 0; i < jobs.length; i++) {
-
-			int start = jobs[i][0];
-			int end = jobs[i][1];
+			int start = jobs[i][0] + beforeTaskEnd;
+			int end = start + jobs[i][1];
 			int diff = end - start;
-
-			TaskVO tVO = new TaskVO(start, end, diff, 0);
+			TaskVO tVO = new TaskVO(start, end, diff, beforeTaskEnd);
 			taskList.add(tVO);
 
+			int index = i + 1;
+			beforeTaskEnd = end - index;
 		}
 		
-		Collections.sort(taskList, new Comparator<TaskVO>() {
-			@Override
-			public int compare(TaskVO o1, TaskVO o2) {
-				if(o1.getDiff() > o2.getDiff()) {
-					return 1;
-				}else if(o1.getDiff() < o2.getDiff()) {
-					return -1;
-				}else {
-					return 0;
-				}
-			}
-		});
+		int sum = 0;
+		for(TaskVO vo : taskList)
+			sum += vo.getDelay()+vo.getDiff();
+		answer = sum/taskList.size();
 		
-		for(TaskVO tVO : taskList) {
-			System.out.println(tVO.getStart() + " ");
-		}
+//		int[] caseArr = new int[taskList.size()];
+//		boolean[] visited = new boolean[taskList.size()];
+//		for (int i = 0; i < taskList.size(); i++) {
+//			caseArr[i] = i;
+//		}
 		
+//		List<String> makeNumber = new ArrayList<String>();
+//		dfs(0, caseArr, new int[taskList.size()], visited, makeNumber);
+
+//		for (String caseNumStr : makeNumber) {
+//			String[] spNum = caseNumStr.split("");
+//			
+//			System.out.println(caseNumStr);
+//			int sum = 0;
+//			for(String num : spNum) {
+//				System.out.println(num);
+//				sum += taskList.get(Integer.parseInt(num)).getDelay()+taskList.get(Integer.parseInt(num)).getDiff();
+//			}
+//			System.out.println(sum/taskList.size());
+//		}
+//		answer = sum/taskList.size();
 
 		return answer;
+	}
+
+	public static void dfs(int n, int[] arr, int[] result, boolean[] visit, List<String> makeNumber) {
+		if (n == arr.length) {// 입력된 숫자 길이 만큼의 경우의 숫자들
+
+			String caseNumStr = "";
+			for (int i = 0; i < arr.length; i++) {
+				caseNumStr += result[i];
+			}
+			makeNumber.add(caseNumStr);
+
+		} else {
+
+			for (int i = 0; i < arr.length; i++) {
+				if (!visit[i]) {
+					visit[i] = true;
+					result[n] = arr[i];
+					dfs(n + 1, arr, result, visit, makeNumber);
+					visit[i] = false;
+				}
+			}
+
+		}
 	}
 
 }
@@ -68,7 +103,7 @@ class TaskVO {
 		this.diff = diff;
 		this.delay = delay;
 	}
-	
+
 	public int getDiff() {
 		return diff;
 	}
